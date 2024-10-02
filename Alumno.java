57975d2483a460c78b243c6cf607e41ead8300dc
Brian <dev.brianoctavio@gmail.com>
2024-10-02 11:47:38 -0600

    02 create class Alumno

---
+++ Alumno.java
@@ -0,0 +1,18 @@
+public class Alumno {
+    private String nombre;
+    private String matricula;
+    private String ingles;
+    private String Iot;
+    private String estructuras;
+
+    //constructor
+    public Alumno(String nombre, String matricula, String ingles, String Iot, String estructuras) {
+        this.nombre = nombre;
+        this.matricula = matricula;
+        this.ingles = ingles;
+        this.Iot = Iot;
+        this.estructuras = estructuras;
+    }
+
+    //getters y setters
+}
